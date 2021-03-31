c3a3adb8800f1cdfcd751371b60f4072c0c4952a
Miguel Badany Cerne <46269793+Cerne17@users.noreply.github.com>
2021-03-31 19:48:33 -0300

    SImple taxes workinfg
    fix compost taxes function

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function Main() {
+function main() {
 
     document.getElementById('secondContainer').removeAttribute("style");
 
-    var capitalSimples = document.getElementById('capitalSimples');
-    var taxaSimples = document.getElementById('taxaSimples');
-    var parcelasSimples = document.getElementById('parcelasSimples');
+    var capitalSimples = document.getElementById('capitalSimples').value;
+    var taxaSimples = document.getElementById('taxaSimples').value;
+    var parcelasSimples = document.getElementById('parcelasSimples').value;
 
-    var capitalComposto = document.getElementById('capitalComposto');
-    var taxaComposta = document.getElementById('taxaComposta');
-    var parcelasCompostas = document.getElementById('parcelasCompostas');
+    var capitalComposto = document.getElementById('capitalComposto').value;
+    var taxaComposta = document.getElementById('taxaComposta').value;
+    var parcelasCompostas = document.getElementById('parcelasCompostas').value;
+    // console.log(capitalSimples, taxaSimples, parcelasSimples, capitalComposto, taxaComposta, parcelasCompostas)
 
     var montanteSimples = (capitalSimples * taxaSimples * parcelasSimples) + capitalSimples;
     var jurosSimples = capitalSimples * taxaSimples * parcelasSimples;
@@ -16,21 +17,22 @@ function Main() {
     var montanteComposto = capitalComposto * Math.pow((1 + taxaComposta), parcelasCompostas);
     var jurosCompostos = capitalComposto * Math.pow((1 + taxaComposta), parcelasCompostas) - capitalComposto;
 
-    var infoSimpleMontant = getElementById("infoOfSimpleMontant");
-    var infoSimpleTaxes = getElementById("infoOfSimpleTaxes");
+    montanteSimples = parseFloat(montanteSimples).toFixed(2);
+    jurosSimples = parseFloat(jurosSimples).toFixed(2);
 
-    var infoCompMontant = getElementById("infoOfCompMontant");
-    var infoCompTaxes = getElementById("infoOfCompTaxes");
+    montanteComposto = parseFloat(montanteComposto).toFixed(2);
+    jurosCompostos = parseFloat(jurosCompostos).toFixed(2);
 
-    infoSimpleMontant.innerHTML = montanteSimples;
-    infoSimpleTaxes.innerHTML = jurosSimples;
+    var infoSimpleMontant = document.getElementById("infoOfSimpleMontant");
+    var infoSimpleTaxes = document.getElementById("infoOfSimpleTaxes");
+
+    var infoCompMontant = document.getElementById("infoOfCompMontant");
+    var infoCompTaxes = document.getElementById("infoOfCompTaxes");
+
+    infoSimpleMontant.innerHTML = "R$"+montanteSimples;
+    infoSimpleTaxes.innerHTML = "R$"+jurosSimples;
     
-    infoCompMontant.innerHTML = montanteComposto;
-    infoCompTaxes.innerHTML = jurosCompostos;
-
-    console.log(montanteSimples);
-    console.log(jurosSimples);
-    console.log(montanteComposto);
-    console.log(jurosCompostos);
+    infoCompMontant.innerHTML = "R$"+montanteComposto;
+    infoCompTaxes.innerHTML = "R$"+jurosCompostos;
 
 }
